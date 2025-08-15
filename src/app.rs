d45d51df95204c9692e2794de2b47588d2a57f32
Michael Nedokushev <grouzen.hexy@gmail.com>
2025-08-15 23:27:07 +0100

    Display overview info in title panel

--- src/app.rs
+++ src/app.rs
@@ -78,6 +78,57 @@ impl App {
             .borders(Borders::ALL)
             .border_type(BorderType::Rounded);
 
+        let [
+            smbios_version_area,
+            charging_status_area,
+            charge_percentage_area,
+            max_charge_limit_area,
+        ] = Layout::horizontal([
+            Constraint::Max(6),
+            Constraint::Max(13),
+            Constraint::Max(4),
+            Constraint::Max(9),
+        ])
+        .horizontal_margin(2)
+        .spacing(2)
+        .areas(block.inner(area));
+
+        // BIOS version
+        if let Some(smbios_version) = self.framework.controls.smbios_version() {
+            frame.render_widget(
+                Paragraph::new(format!("v{}", smbios_version)),
+                smbios_version_area,
+            );
+        }
+
+        let charge_percentage = self.framework.controls.charge_percentage();
+        let charge_style = match charge_percentage {
+            Some(charge_percentage) if charge_percentage < 15 => Style::new().red(),
+            _ => Style::new().green(),
+        };
+
+        // Charging status
+        frame.render_widget(
+            Paragraph::new(self.framework.controls.charging_status()).style(charge_style),
+            charging_status_area,
+        );
+
+        // Charge percentage
+        if let Some(charge_percentage) = self.framework.controls.charge_percentage() {
+            frame.render_widget(
+                Paragraph::new(format!("{}%", charge_percentage)).style(charge_style),
+                charge_percentage_area,
+            );
+        }
+
+        // Max charge limit
+        if let Some(max_charge_limit) = self.framework.controls.max_charge_limit() {
+            frame.render_widget(
+                Paragraph::new(format!("Max: {}%", max_charge_limit)),
+                max_charge_limit_area,
+            );
+        }
+
         frame.render_widget(block, area);
     }
 
@@ -219,15 +270,6 @@ impl App {
     }
 
     fn render_charge_level(&self, frame: &mut Frame, key_area: Rect, value_area: Rect) {
-        let is_charging = self.framework.controls.is_charging();
-        let is_ac_connected = self.framework.controls.is_ac_connected();
-
-        let status = match (is_charging, is_ac_connected) {
-            (true, true) => "Charging",
-            (false, true) => "Fully charged",
-            (false, false) => "Discharging",
-            (true, false) => "???",
-        };
         let gauge = match self.framework.controls.charge_percentage() {
             Some(charge_percentage) => {
                 let gauge_style = if charge_percentage < 15 {
@@ -235,7 +277,11 @@ impl App {
                 } else {
                     Style::new().green().on_gray()
                 };
-                let label = format!("{} {}%", status, charge_percentage);
+                let label = format!(
+                    "{} {}%",
+                    self.framework.controls.charging_status(),
+                    charge_percentage
+                );
 
                 Gauge::default()
                     .percent(charge_percentage as u16)

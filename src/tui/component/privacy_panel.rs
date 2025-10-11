45274508d90476fdf9a3878e04e0047d355c6f63
Michael Nedokushev <grouzen.hexy@gmail.com>
2025-10-11 19:56:10 +0100

    Change look&feel of privacy panel (#21)
    * Change look&feel of privacy panel
    
    * Remove tui-experiment crate

--- src/tui/component/privacy_panel.rs
+++ src/tui/component/privacy_panel.rs
@@ -2,9 +2,8 @@ use ratatui::{
     Frame,
     layout::{Constraint, Layout, Rect},
     style::Style,
-    widgets::{Block, BorderType, Borders},
+    widgets::{Block, BorderType, Borders, Paragraph},
 };
-use tui_framework_experiment::toggle_switch::{State, ToggleSwitch};
 
 use crate::{
     framework::info::FrameworkInfo,
@@ -14,26 +13,40 @@ use crate::{
 pub struct PrivacyPanelComponent;
 
 impl PrivacyPanelComponent {
-    fn render_mic(&self, frame: &mut Frame, area: Rect, info: &FrameworkInfo) {
-        let toggle_state = if info.is_microphone_enabled {
-            State::On
+    fn render_mic(
+        &self,
+        frame: &mut Frame,
+        key_area: Rect,
+        value_area: Rect,
+        theme: &Theme,
+        info: &FrameworkInfo,
+    ) {
+        let toggle = if info.is_microphone_enabled {
+            Paragraph::new("ON").style(Style::default().fg(theme.indication_ok))
         } else {
-            State::Off
+            Paragraph::new("OFF").style(Style::default().fg(theme.indication_warning))
         };
-        let toggle = &ToggleSwitch::new("Microphone", toggle_state);
 
-        frame.render_widget(toggle, area);
+        frame.render_widget(Paragraph::new("Microphone"), key_area);
+        frame.render_widget(toggle, value_area);
     }
 
-    fn render_camera(&self, frame: &mut Frame, area: Rect, info: &FrameworkInfo) {
-        let toggle_state = if info.is_camera_enabled {
-            State::On
+    fn render_camera(
+        &self,
+        frame: &mut Frame,
+        key_area: Rect,
+        value_area: Rect,
+        theme: &Theme,
+        info: &FrameworkInfo,
+    ) {
+        let toggle = if info.is_camera_enabled {
+            Paragraph::new("ON").style(Style::default().fg(theme.indication_ok))
         } else {
-            State::Off
+            Paragraph::new("OFF").style(Style::default().fg(theme.indication_warning))
         };
-        let toggle = &ToggleSwitch::new("Camera", toggle_state);
 
-        frame.render_widget(toggle, area);
+        frame.render_widget(Paragraph::new("Camera"), key_area);
+        frame.render_widget(toggle, value_area);
     }
 }
 
@@ -45,20 +58,35 @@ impl Component for PrivacyPanelComponent {
             .border_style(Style::default().fg(theme.border))
             .border_type(BorderType::Rounded);
 
-        let [mic_area, camera_area] =
-            Layout::vertical([Constraint::Length(1), Constraint::Length(1)])
-                .spacing(1)
+        let [keys_area, values_area] =
+            Layout::horizontal([Constraint::Fill(1), Constraint::Fill(1)])
                 .horizontal_margin(2)
+                .spacing(1)
                 .vertical_margin(1)
                 .areas(block.inner(area));
 
+        let keys_block = Block::default().borders(Borders::NONE);
+        let values_block = Block::default().borders(Borders::NONE);
+
+        let [mic_key_area, camera_key_area] =
+            Layout::vertical([Constraint::Length(1), Constraint::Length(1)])
+                .spacing(1)
+                .areas(keys_block.inner(keys_area));
+        let [mic_value_area, camera_value_area] =
+            Layout::vertical([Constraint::Length(1), Constraint::Length(1)])
+                .spacing(1)
+                .areas(values_block.inner(values_area));
+
         // Micrhophone
-        self.render_mic(frame, mic_area, info);
+        self.render_mic(frame, mic_key_area, mic_value_area, theme, info);
 
         // Camera
-        self.render_camera(frame, camera_area, info);
+        self.render_camera(frame, camera_key_area, camera_value_area, theme, info);
 
-        // Render block
+        // Render blocks
+        frame.render_widget(keys_block, keys_area);
+        frame.render_widget(values_block, values_area);
+
         frame.render_widget(block, area);
     }
 }

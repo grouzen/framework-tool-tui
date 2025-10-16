b27af5d96aa33ce1f5634b509a86ae5a55fbdb9c
Michael Nedokushev <grouzen.hexy@gmail.com>
2025-10-16 22:27:56 +0200

    Check admin permissions on startup (#41)
    * Check admin permissions on startup
    
    * Bump cargo version to 0.5.3

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use framework_tool_tui::app::App;
+use uzers::get_current_uid;
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
+
+    if !check_permissions() {
+        return Err(color_eyre::Report::msg(
+            "The application needs to be run with root privileges.",
+        ));
+    }
 
     let mut terminal = ratatui::init();
     let mut app = App::new();
@@ -12,4 +19,11 @@ async fn main() -> color_eyre::Result<()> {
     ratatui::restore();
 
     result
+}
+
+fn check_permissions() -> bool {
+    #[cfg(target_os = "linux")]
+    let is_admin = get_current_uid() == 0;
+
+    is_admin
 }

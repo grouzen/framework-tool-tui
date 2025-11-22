839421c5a888ff247ab17d4130d9b240d7325fcf
Daniel Schaefer <git@danielschaefer.me>
2025-11-22 16:20:38 +0800

    windows: Build without uzers (#60)
    Instead of including windows dependencies, just check if it works and
    report an error.
    
    If the driver is not available for that platform. The error message from
    framework_lib will say that. That's why I also print that.
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 use framework_tool_tui::app::App;
+#[cfg(unix)]
 use uzers::get_current_uid;
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
-    if !check_permissions() {
-        return Err(color_eyre::Report::msg(
-            "The application needs to be run with root privileges.",
-        ));
-    }
+    check_permissions()?;
 
     let mut terminal = ratatui::init();
     let mut app = App::new()?;
@@ -21,9 +18,27 @@ async fn main() -> color_eyre::Result<()> {
     result
 }
 
-fn check_permissions() -> bool {
-    #[cfg(any(target_os = "linux", target_os = "freebsd"))]
+#[cfg(unix)]
+fn check_permissions() -> color_eyre::Result<()> {
     let is_admin = get_current_uid() == 0;
 
-    is_admin
+    if !is_admin {
+        return Err(color_eyre::Report::msg(
+            "The application needs to be run with root privileges.",
+        ));
+    }
+
+    Ok(())
+}
+
+#[cfg(windows)]
+fn check_permissions() -> color_eyre::Result<()> {
+    if let Err(err) = framework_lib::chromium_ec::CrosEc::new().version_info() {
+        return Err(color_eyre::Report::msg(format!(
+            "The application needs to be run as admin: {:?}.",
+            err
+        )));
+    }
+
+    Ok(())
 }

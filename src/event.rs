3ab19d5ad07be5e948a15d95014afc433ffaf816
Michael Nedokushev <grouzen.hexy@gmail.com>
2025-12-26 19:35:36 +0100

    feat: support changing tick interval

--- src/event.rs
+++ src/event.rs
@@ -3,7 +3,7 @@ use std::time::Duration;
 use color_eyre::eyre::Report;
 use crossterm::event::EventStream;
 use futures::{FutureExt, StreamExt};
-use tokio::sync::mpsc;
+use tokio::sync::{mpsc, watch};
 
 pub enum Event {
     Tick,
@@ -13,6 +13,7 @@ pub enum Event {
 pub struct EventLoop {
     tx: mpsc::UnboundedSender<Event>,
     rx: mpsc::UnboundedReceiver<Event>,
+    interval_tx: watch::Sender<Duration>,
 }
 
 impl Default for EventLoop {
@@ -24,12 +25,18 @@ impl Default for EventLoop {
 impl EventLoop {
     pub fn new() -> Self {
         let (tx, rx) = mpsc::unbounded_channel();
+        let (interval_tx, _interval_rx) = watch::channel(Duration::from_millis(1000));
 
-        Self { tx, rx }
+        Self {
+            tx,
+            rx,
+            interval_tx,
+        }
     }
 
     pub fn run(&self, tick_interval: Duration) {
         let tx = self.tx.clone();
+        let mut interval_rx = self.interval_tx.subscribe();
 
         tokio::spawn(async move {
             let mut event_stream = EventStream::new();
@@ -46,9 +53,17 @@ impl EventLoop {
                     Some(Ok(event)) = input_event => {
                         let _ = tx.send(Event::Input(event));
                     }
+                    Ok(()) = interval_rx.changed() => {
+                        let new_interval = *interval_rx.borrow_and_update();
+                        tick = tokio::time::interval(new_interval);
+                    }
                 }
             }
         });
+    }
+
+    pub fn set_tick_interval(&self, tick_interval: Duration) {
+        let _ = self.interval_tx.send(tick_interval);
     }
 
     pub async fn next(&mut self) -> color_eyre::Result<Event> {
